f307b642b30eb6e040a8d580419562e3bf5525c1
Alros Avellana <alros.avellana@yahoo.com>
2020-04-17 14:12:27 -0700

    fixed JSON error and removed sensetive data

--- index.js
+++ index.js
@@ -11,24 +11,23 @@ exports.handler =  function(event, context, callback) {
   https.get(url, (res) => {
       
       res.on("data", chunk => {
-          quote+=chunk;
-          quote = JSON.parse(quote);
+          quote = JSON.parse(chunk);
           quoteMessage = quote.quoteText + " ~" + quote.quoteAuthor   ;
       });
     callback(null, res.statusCode);
   }).on('error', (e) => {
     callback(Error(e));
   });
-  console.log(quoteMessage)
-  /*//SMS Publish Code*/
+  console.log(quoteMessage);
+  
+    /*//SMS Publish Code*/
   var params = {
   Message:`  ${quoteMessage}  `, /* required */
   Subject: 'Daily Quote',
-  TopicArn: 'arn:aws:sns:us-east-1:309793080404:Daily_Quote'
+  TopicArn:// <insert ARN here>
 };
 sns.publish(params, function(err, data) {
   if (err) console.log(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
 });
-  
-}
+};
