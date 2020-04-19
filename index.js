cace7f2927810ad091b1734e81620e017a53bded
Alros Avellana <alros.avellana@yahoo.com>
2020-04-18 20:56:29 -0700

    moved sns function into http request quote due to the issue of sns function finishing before quote was retrieved from api

--- index.js
+++ index.js
@@ -4,30 +4,33 @@ var sns = new AWS.SNS();
 const https = require('https')
 let url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
 let quote = "";
-
 let quoteMessage = "";
 
 exports.handler =  function(event, context, callback) {
   https.get(url, (res) => {
-      
       res.on("data", chunk => {
+          console.log(`quote received from API - ` + chunk)
           quote = JSON.parse(chunk);
-          quoteMessage = quote.quoteText + " ~" + quote.quoteAuthor   ;
-      });
-    callback(null, res.statusCode);
+          quoteMessage = quote.quoteText + " ~" + quote.quoteAuthor;
+          
+          var params = {
+                Message: `${quoteMessage}`,
+                Subject: 'Daily Quote',
+                TopicArn: 'insert ARN HERE'
+                
+                //PhoneNumber:
+              };
+              
+          sns.publish(params, function(err, data) {
+            console.log(`sns is publishing`)
+            if (err) console.log(err, err.stack); // an error occurred
+            else     console.log(`successful`);           // successful response
+           });
+  
+        });
+        
+      callback(null, res.statusCode);
   }).on('error', (e) => {
     callback(Error(e));
   });
-  console.log(quoteMessage);
-  
-    /*//SMS Publish Code*/
-  var params = {
-  Message:`  ${quoteMessage}  `, /* required */
-  Subject: 'Daily Quote',
-  TopicArn:// <insert ARN here>
-};
-sns.publish(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
 };
